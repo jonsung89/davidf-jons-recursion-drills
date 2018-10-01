3379d5a6f17ebeab8a0e97805cbd656a322a00a6
David Folks <aldwyn.wot@gmail.com>
2018-10-01 10:20:52 -0600

    doubling the array

--- countingsheep.js
+++ countingsheep.js
@@ -1,9 +1,19 @@
 'use strict';
+// COUNTING SHEEP
 
-function countsheeps(n) {
-  if (n === 0) return 0;
-  console.log(n + ' - Another sheep jump over the fence');
-  countsheeps(n-1);
+// function countsheeps(n) {
+//   if (n === 0) return 0;
+//   console.log(n + ' - Another sheep jump over the fence');
+//   countsheeps(n-1);
+// }
+
+// countsheeps(3);
+
+//DOUBLING AN ARRAY
+
+function doubleArr(arr) {
+  if (arr.length === 0) return [];
+  return [arr[0] * 2, ...doubleArr(arr.slice(1))];
 }
 
-countsheeps(3);
+console.log(doubleArr([1, 2, 3]));
